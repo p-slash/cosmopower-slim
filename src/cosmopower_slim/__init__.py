40e28007018e8793f966ca4cd1ba21a939f0f8aa
Naim Goksel Karacayli <ngokselk@gmail.com>
2025-09-09 18:42:43 -0400

    slim down cosmopower

--- src/cosmopower_slim/__init__.py
+++ src/cosmopower_slim/__init__.py
@@ -1,7 +1,2 @@
-from .cosmopower_PCA import cosmopower_PCA
-from .cosmopower_PCAplusNN import cosmopower_PCAplusNN
-from .cosmopower_NN import cosmopower_NN
-from .likelihoods import *
-
-__version__ = "0.1.3"
-__author__ = 'Alessio Spurio Mancini'
+__version__ = "0.1.0"
+__author__ = 'Naim Goksel Karacayli'
